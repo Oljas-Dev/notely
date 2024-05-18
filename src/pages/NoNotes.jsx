f3e9d96fbe1d8e690d9138ff39984ef703694841
Oljas-Dev <150025424+Oljas-Dev@users.noreply.github.com>
2024-05-18 23:20:43 +0200

    Update notes

--- src/pages/NoNotes.jsx
+++ src/pages/NoNotes.jsx
@@ -1,5 +1,6 @@
 import styles from "./NoNotes.module.css";
 import PropTypes from "prop-types";
+import notesPicture from "../../public/notes.png";
 
 NoNotes.propTypes = {
   type: PropTypes.string,
@@ -9,24 +10,14 @@ function NoNotes({ type }) {
   if (type === "noCheckedNotes")
     return (
       <div className={styles.flex}>
-        <img
-          src="../../public/notes.png"
-          width="206px"
-          height="188px"
-          alt="no notes"
-        />
+        <img src={notesPicture} width="206px" height="188px" alt="no notes" />
         <p className={styles.text}>You don’t have any completed notes</p>
       </div>
     );
 
   return (
     <div className={styles.flex}>
-      <img
-        src="../../public/notes.png"
-        width="206px"
-        height="188px"
-        alt="no notes"
-      />
+      <img src={notesPicture} width="206px" height="188px" alt="no notes" />
       <p className={styles.text}>You don’t have any notes</p>
     </div>
   );
